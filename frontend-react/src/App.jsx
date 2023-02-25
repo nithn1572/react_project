c249d77ef7209b93f20b0cf99fa944c26fef1a33
nithin <nithinreddy1572@gmail.com>
2023-02-25 21:56:05 +0530

    removed errors

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,7 +1,4 @@
-import { useEffect,useState} from 'react'
-import reactLogo from './assets/react.svg'
-// import './App.css'
-import ComponentOne from './ComponentOne'
+import { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,56 +6,49 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
+
 function App() {
-
-  // let number = 0;
-
-  // const [number,setNumber] = useState(0);
-
-  // const increaseCount = () => {
-  //   // number+=1;
-  //   setNumber(number+1);
-  // }
-
-  // useEffect(()=>{
-  //   console.log("This component has mounted")
-  // },[])
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="APP">
-      <Navbar bg="light" variant="light">
+    <div className="App">
+       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
           <Button variant="primary"><i class="fas fa-moon fa-lg fa-fw"></i></Button>{' '}
+
           </Nav>
         </Container>
       </Navbar>
-      <h1 className="text-center">TODO APP</h1>
+      <h1 className="text-center">Hello world </h1>
       <div style={{
-            display: "flex",
-            alignItems : "center",
-            justifyContent :"center"
-        }}> 
-      <InputGroup style={{width:"400px"}} className="mb-3">
+    display: "flex",
+    alignItems : "center",
+    justifyContent :"center"
+}}> 
+
+
+
+      <InputGroup style={{width:"300px"}} className="mb-3">
         <Form.Control
           placeholder="Recipient's username"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
         />
-        <Button variant="outline-primary" id="button-addon2">
-          <i class="fa fa-add"> </i>
+        <Button variant="outline-success" id="button-addon2">
+        <i class="fa fa-add"></i>
         </Button>
       </InputGroup>
       </div>
       <div className="mt-4">
                 <Nav justify variant="tabs" defaultActiveKey="?list=BLIND75">
-                    <Nav.Item>
+                    <Nav.Item >
                         <Nav.Link eventKey="?list=BLIND75">
                             <span className="tab-font">Todos</span>
                         </Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item >
                         <Nav.Link eventKey="link-1">
                             <span className="tab-font">Completed</span>
                         </Nav.Link>
